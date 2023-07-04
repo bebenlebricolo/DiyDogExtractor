9154f48a004ae96f903e684764fe5333ba796633
bebenlebricolo <benoittarrade@hotmail.fr>
2023-07-04 10:02:21 +0700

    added round_trip script to automate tasks
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

---
+++ round_trip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script automates the 4 major steps in
+# database extraction / cleanup performed by the tools provided.
+
+source .venv/bin/activate
+
+echo "################# Starting up db extraction tool #################"
+python -m Sources.dbextractor false true true
+
+echo -e "\n################# Starting up db patching tool #################"
+python -m Sources.ScriptingTools.patcher Patches Sources/.cache/deployed/recipes
+
+echo -e "\n################# Starting up db cleanup tool #################"
+python -m Sources.dbsanitizer References Sources/.cache/deployed Sources/.cache/deployed/dbsanitizer
+
+echo -e "\n################# Overwriting deployed all_recipes.json with cleaned up version #################"
+cp Sources/.cache/deployed/dbsanitizer/all_recipes.json Sources/.cache/deployed/recipes/all_recipes.json
+echo "-> Ok !"
+
+echo -e "\n################# Starting up db analysis tool #################"
+python -m Sources.dbanalyser Sources/.cache/deployed/recipes/all_recipes.json Sources/.cache/deployed/dbanalysis
+echo -e "\n################# All steps done ! #################"
